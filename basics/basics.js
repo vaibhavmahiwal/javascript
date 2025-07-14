4899fa9e3509c9d069c07e80759d4f87055ea4e1
vaibhavmahiwal <vaibhavmahiwal75@gmail.com>
2025-07-14 19:17:52 +0000

    done

---
+++ basics/basics.js
@@ -0,0 +1,12 @@
+const accountId="23456"
+let accountemail="vaibhav@google.com"
+var accountpassword="12345"
+accountcity="jaipur"
+let accountstate
+
+accountemail="abc@abc.com"
+accountpassword="767676"
+accountcity="banglore"
+
+console.log(accountId)
+console.table([accountId,accountemail,accountpassword,accountcity,accountstate])
