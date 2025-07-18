ace1a9384cfc7b95f640f89c7af17ae85de8e27f
vaibhavmahiwal <vaibhavmahiwal75@gmail.com>
2025-07-18 17:20:07 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,23 @@
+"use strict"
+console.log(3+2)
+console.log("vaibhav")
+
+
+let name="vaibhav"
+let age=18
+let isloggedin=true
+
+//number=2^53
+//bigint
+//string=""
+//boolean=true/false
+//null= a standalone value 
+//object
+console.log(typeof "vaibhav")
+console.log(typeof age)
+console.log(typeof null)//object
+console.log(typeof undefied)//undefined
+
+
+
+
